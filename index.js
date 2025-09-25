52ff3f89497de4b5d3db3404db50186c5cf6a826
fay <hisakiryouhei@gmail.com>
2025-09-25 15:03:56 +0700

    real done

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-let discountButton = document.getElementById("discount-button");
-let message = document.getElementById("message");
+let numberInput = document.getElementById("number-input");
+let generateButton = document.getElementById("generate-button");
+let output = document.getElementById("output");
 
-function showMessage() {
-    message.innerHTML = "หมดเวลาสนุกแล้วสิ";
+function showTable() {
+    number = Number(numberInput.value);
+    output.innerHTML = "";
+
+    if(number == 0) {
+        output.innerHTML = "0 Times Table does not provide";
+        return;
+    }
+
+    let result = "";
+    for (let i = 1; i <= 12; i++) {
+        result += String(number) + " x " + String(i) + " = " + String(number * i) + "<br>";
+    } 
+        
+    output.innerHTML = result;
 }
 
-discountButton.addEventListener("click", showMessage);
+generateButton.addEventListener("click", showTable); 
